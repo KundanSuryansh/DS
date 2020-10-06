34db0740528bd96b99d5066371d6ff52560b4255
Kundan Suryansh <suryansh100.ks@gmail.com>
2020-10-07 01:02:31 +0530

    implementation of insert at any position data functionality.

--- src/com/linear/ds/SinglyLL.java
+++ src/com/linear/ds/SinglyLL.java
@@ -1,5 +1,8 @@
 package com.linear.ds;
 
+/**
+ * @author Kundan Suryansh
+ */
 class Node<T> {
     private T data;
     private Node link;
@@ -54,7 +57,7 @@ class SinglyLinkList<T> {
             System.out.print(curNode.getData() + ", ");
             curNode = curNode.getLink();
         }
-        System.out.println(curNode.getData() + " ]" );
+        System.out.println(curNode.getData() + " ]");
     }
 
     public void insertFromFront(T data) {
@@ -80,22 +83,65 @@ class SinglyLinkList<T> {
         }
     }
 
-    public void searchAnObject(T data){
-        if(head == null){
+    public void searchAnObject(T data) {
+        boolean isElementPresent = false;
+        if (head == null) {
             System.out.println("No object are inserted into Link List");
             return;
         }
         int pos = 1;
         Node<T> curNode = head;
-        while (curNode.getLink() != null) {
-            if(curNode.getData().equals(data)){
+        while (curNode != null) {
+            if (curNode.getData().equals(data)) {
+                isElementPresent = true;
                 break;
             }
             curNode = curNode.getLink();
             pos++;
         }
-        if(curNode.getData().equals(data)){
-            System.out.println("Element found at position : "+pos);
+        if (isElementPresent) {
+            System.out.println("Element found at position : " + pos);
+        } else {
+            System.out.println("Element is not present");
+        }
+
+    }
+
+    /**
+     * @param data : object which we want to store.
+     * @param pos  : It should start from 1. if pos value is greater than length of link list, it will store data at last.
+     */
+    public void insertAtAnyPosition(T data, int pos) {
+        if(pos < 1) {
+            System.out.println("Position value is not proper.");
+            return;
+        }
+        Node<T> newNode = new Node<T>(data);
+        boolean isElementInserted = false;
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        if (pos == 1) {
+            newNode.setLink(head);
+            head = newNode;
+        } else {
+            int count = 1;
+            Node<T> curNode = head;
+            Node<T> prevNode = head;
+            while (curNode != null) {
+                if (count == pos) {
+                    prevNode.setLink(newNode);
+                    newNode.setLink(curNode);
+                    isElementInserted = true;
+                    break;
+                }
+                prevNode = curNode;
+                curNode = curNode.getLink();
+                count++;
+            }
+            if(!isElementInserted)
+                prevNode.setLink(newNode);
         }
 
     }
@@ -104,7 +150,6 @@ class SinglyLinkList<T> {
 
 public class SinglyLL {
     public static void main(String[] args) {
-
         SinglyLinkList<Integer> singlyLinkList = new SinglyLinkList<>();
         singlyLinkList.createSinglyLinkList(new Integer[]{1, 2, 4, 5, 3});
         singlyLinkList.printSinglyLinkList();
@@ -112,7 +157,9 @@ public class SinglyLL {
         singlyLinkList.printSinglyLinkList();
         singlyLinkList.insertFromBack(new Integer(23));
         singlyLinkList.printSinglyLinkList();
-        singlyLinkList.searchAnObject(23);
+        singlyLinkList.searchAnObject(32);
+        singlyLinkList.insertAtAnyPosition(new Integer(56),8);
+        singlyLinkList.printSinglyLinkList();
     }
 
 }
